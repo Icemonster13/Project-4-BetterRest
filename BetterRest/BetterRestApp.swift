d5561cbec6fe450897fcdcde67ced50630462eec
Michael Pascucci <mdpascucci@live.com>
2022-05-14 08:25:00 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Michael & Diana Pascucci on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
